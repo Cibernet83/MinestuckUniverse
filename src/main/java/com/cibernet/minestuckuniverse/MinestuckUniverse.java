6a71e20f202ee761ef597c0aeda7826ca8bfbbc2
Cibernet <Cibernet83@users.noreply.github.com>
2019-02-14 13:54:11 -0500

    Setup

---
+++ src/main/java/com/cibernet/minestuckuniverse/MinestuckUniverse.java
@@ -0,0 +1,31 @@
+package com.cibernet.minestuckuniverse;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = MinestuckUniverse.MODID, name = MinestuckUniverse.NAME, version = MinestuckUniverse.VERSION)
+public class MinestuckUniverse
+{
+    public static final String MODID = "minestuckuniverse";
+    public static final String NAME = "Minestuck Universe";
+    public static final String VERSION = "@VERSION@";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
