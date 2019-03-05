e02892d5e5b4c65365d500e9cefc0ac5785b6600
Cibernet <Cibernet83@users.noreply.github.com>
2019-03-05 14:59:37 -0500

    Foci Base

---
+++ src/main/java/com/cibernet/minestuckuniverse/foci/MSUFoci.java
@@ -0,0 +1,12 @@
+package com.cibernet.minestuckuniverse.foci;
+
+import thaumcraft.api.casters.FocusEngine;
+
+public class MSUFoci
+{
+    public static void registerFoci()
+    {
+        //FocusEngine.registerElement();
+    }
+
+}
