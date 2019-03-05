26c377946526f3f01d7c4e9e6e110cc050e61326
Cibernet <Cibernet83@users.noreply.github.com>
2019-03-05 15:00:01 -0500

    Added Sburbomancy tab to the Thaumonomicon

--- src/main/java/com/cibernet/minestuckuniverse/proxy/CommonProxy.java
+++ src/main/java/com/cibernet/minestuckuniverse/proxy/CommonProxy.java
@@ -11,6 +11,8 @@ import com.mraof.minestuck.util.KindAbstratusType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.research.ResearchCategories;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +34,7 @@ public class CommonProxy
         List<KindAbstratusType> abstrata = KindAbstratusList.getTypeList();
         if(MinestuckUniverse.isThaumLoaded)
         {
+            ResearchCategories.registerCategory("SBURBOMANCY", "FIRSTSTEPS", new AspectList(), new ResourceLocation("minestuckuniverse", "textures/gui/tab.png"), new ResourceLocation("minestuckuniverse", "textures/gui/gui_research_bg.jpg"));
             ThaumcraftApi.registerResearchLocation(new ResourceLocation("minestuckuniverse", "research/sburb.json"));
 
 
