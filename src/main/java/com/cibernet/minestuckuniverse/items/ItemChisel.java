1c92371a4d7bc9bfb7b171ec9e810a77712c731f
Cibernet <Cibernet83@users.noreply.github.com>
2019-12-05 11:47:18 -0500

    Zillystone Shard yields are now affected by Luck/Unluck

--- src/main/java/com/cibernet/minestuckuniverse/items/ItemChisel.java
+++ src/main/java/com/cibernet/minestuckuniverse/items/ItemChisel.java
@@ -3,13 +3,18 @@ package com.cibernet.minestuckuniverse.items;
 import com.cibernet.minestuckuniverse.MinestuckUniverse;
 import com.cibernet.minestuckuniverse.blocks.MinestuckUniverseBlocks;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class ItemChisel extends MSUItemBase
 {
@@ -25,7 +30,14 @@ public class ItemChisel extends MSUItemBase
 	{
 		if(worldIn.getBlockState(pos).getBlock().equals(MinestuckUniverseBlocks.zillyStone) && !worldIn.isRemote)
 		{
-			InventoryHelper.spawnItemStack(worldIn,pos.getX(),pos.getY(),pos.getZ(), new ItemStack(MinestuckUniverseItems.zillystoneShard));
+			PotionEffect luckFX = player.getActivePotionEffect(MobEffects.LUCK);
+			PotionEffect unluckFX = player.getActivePotionEffect(MobEffects.UNLUCK);
+			int luck = (luckFX != null ? luckFX.getAmplifier()+1 : 0) - (unluckFX != null ? unluckFX.getAmplifier()+1 : 0);
+			int val = 1;
+
+			val += Math.signum(luck)*((new Random().nextInt(10) < Math.abs(luck) ? 1 : 0));
+
+			InventoryHelper.spawnItemStack(worldIn,pos.getX(),pos.getY(),pos.getZ(), new ItemStack(MinestuckUniverseItems.zillystoneShard, val));
 			player.getHeldItem(hand).damageItem(1, player);
 			return EnumActionResult.SUCCESS;
 		}
