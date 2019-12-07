92c61284b3f9dc05de06d7456a3356ab04bf3c33
Cibernet <cyberneticGuardian@gmail.com>
2019-12-07 12:59:39 -0500

    Added Sleeved Transportalizers

---
+++ src/main/java/com/cibernet/minestuckuniverse/blocks/BlockWoolTransportalizer.java
@@ -0,0 +1,20 @@
+package com.cibernet.minestuckuniverse.blocks;
+
+import com.cibernet.minestuckuniverse.TabMinestuckUniverse;
+import com.mraof.minestuck.block.BlockTransportalizer;
+import com.mraof.minestuck.item.TabMinestuck;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.EnumDyeColor;
+
+public class BlockWoolTransportalizer extends BlockTransportalizer
+{
+	public EnumDyeColor color;
+	public BlockWoolTransportalizer(EnumDyeColor color)
+	{
+		super();
+		this.setCreativeTab(TabMinestuckUniverse.instance);
+		this.setUnlocalizedName("woolTransportalizer."+color.getUnlocalizedName());
+		this.setRegistryName(color.getName()+"_wool_transportalizer");
+		this.color = color;
+	}
+}
