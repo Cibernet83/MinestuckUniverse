d09063627f0df248f9c96e48306bb80cff38280c
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2021-08-22 13:23:22 -0500

    Update PropertyExplode.java

--- src/main/java/com/cibernet/minestuckuniverse/items/properties/PropertyExplode.java
+++ src/main/java/com/cibernet/minestuckuniverse/items/properties/PropertyExplode.java
@@ -1,9 +1,16 @@
 package com.cibernet.minestuckuniverse.items.properties;
 
 import com.cibernet.minestuckuniverse.events.handlers.CommonEventHandler;
+import net.minecraft.enchantment.EnchantmentProtection;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
 
 public class PropertyExplode extends WeaponProperty
 {
@@ -22,14 +29,22 @@ public class PropertyExplode extends WeaponProperty
 	public void onEntityHit(ItemStack stack, EntityLivingBase target, EntityLivingBase player)
 	{
 		if(!onCrit && (player.world.rand.nextFloat() <= chance) && (!(player instanceof EntityPlayer) || CommonEventHandler.getCooledAttackStrength(((EntityPlayer) player)) >= 1))
-			target.world.createExplosion(player, target.posX, target.posY, target.posZ, strength, false);
+		{
+			target.hurtResistantTime = 0;
+			target.world.createExplosion(null, target.posX, target.posY, target.posZ, strength, net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(player.world, player));
+		}
 	}
 
 	@Override
 	public float onCrit(ItemStack stack, EntityPlayer player, EntityLivingBase target, float damageModifier)
 	{
 		if(onCrit && (player.world.rand.nextFloat() <= chance) && (!(player instanceof EntityPlayer) || CommonEventHandler.getCooledAttackStrength(((EntityPlayer) player)) >= 1))
-			target.world.createExplosion(player, target.posX, target.posY, target.posZ, strength, false);
+		{
+			target.hurtResistantTime = 0;
+			target.world.createExplosion(null, target.posX, target.posY, target.posZ, strength, net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(player.world, player));
+		}
 		return super.onCrit(stack, player, target, damageModifier);
 	}
+
+
 }
