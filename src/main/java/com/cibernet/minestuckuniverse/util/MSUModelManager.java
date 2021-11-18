4c1f29dc46984187c4b2c13aa5673423953bdc89
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2021-11-18 18:16:27 -0500

    model updates

--- src/main/java/com/cibernet/minestuckuniverse/util/MSUModelManager.java
+++ src/main/java/com/cibernet/minestuckuniverse/util/MSUModelManager.java
@@ -3,6 +3,7 @@ package com.cibernet.minestuckuniverse.util;
 import com.cibernet.minestuckuniverse.MinestuckUniverse;
 import com.cibernet.minestuckuniverse.alchemy.MinestuckUniverseGrist;
 import com.mraof.minestuck.Minestuck;
+import com.mraof.minestuck.MinestuckConfig;
 import com.mraof.minestuck.alchemy.GristType;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.ItemMeshDefinition;
@@ -47,6 +48,10 @@ public class MSUModelManager
         register(royalDeringer, 0, "royal_deringer");
         register(scarletRibbitar, 0, "scarlet_ribbitar");
         register(dDEHammerAxe, 0, "eeeeeeeeeeee");
+        if(MinestuckConfig.oldItemModels)
+            register(fearNoAnvil);
+        else register(fearNoAnvil, 0, "fear_no_anvil");
+        register(grimoire, 0, "grimoire");
         ModelLoader.setCustomModelResourceLocation(crockerSpork, 0, new ModelResourceLocation(Minestuck.MOD_ID+":crocker_spoon", "inventory"));
         ModelLoader.setCustomModelResourceLocation(crockerFork, 0, new ModelResourceLocation(Minestuck.MOD_ID+":crocker_fork", "inventory"));
         ModelLoader.setCustomModelResourceLocation(clawHammer, 0, new ModelResourceLocation(Minestuck.MOD_ID+":claw_hammer_old", "inventory"));
