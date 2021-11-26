602e367ada66f403bd257e462c98383c37aa5d60
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2021-11-26 18:20:50 -0500

    Space Salt now beeg frog

--- src/main/java/com/cibernet/minestuckuniverse/items/ItemSpaceSalt.java
+++ src/main/java/com/cibernet/minestuckuniverse/items/ItemSpaceSalt.java
@@ -5,10 +5,12 @@ import com.mraof.minestuck.block.*;
 import com.mraof.minestuck.tileentity.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -33,5 +35,33 @@ public class ItemSpaceSalt extends MSUItemBase {
             return EnumActionResult.SUCCESS;
         }
         return EnumActionResult.FAIL;
+    }
+
+    @Override
+    public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand)
+    {
+        NBTTagCompound nbt = target.writeToNBT(new NBTTagCompound());
+
+        if(nbt.hasKey("Size"))
+        {
+            float size = nbt.getFloat("Size");
+
+            nbt.setFloat("Size", Math.min(10, size+0.2f*(playerIn.isSneaking() ? -1 : 1)));
+            target.readFromNBT(nbt);
+            nbt = target.writeToNBT(new NBTTagCompound());
+            if(nbt.getFloat("Size") == size)
+            {
+                size = Math.min(10, size+(playerIn.isSneaking() ? -1 : 1));
+                nbt.setFloat("Size", size);
+                if(size <= 0)
+                    target.setDead();
+                target.readFromNBT(nbt);
+            }
+
+            stack.shrink(1);
+            return true;
+        }
+
+        return super.itemInteractionForEntity(stack, playerIn, target, hand);
     }
 }
