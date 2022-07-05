6cd7c647bd5c5ffb95f65f0d9e00341da5adb92f
grandioseComputer <garbedcrustacean@gmail.com>
2022-07-04 19:56:58 -0500

    cantExit works

--- src/main/java/com/cibernet/minestuckuniverse/entity/EntityBubble.java
+++ src/main/java/com/cibernet/minestuckuniverse/entity/EntityBubble.java
@@ -11,7 +11,12 @@ import net.minecraft.world.World;
 import net.minecraftforge.event.world.GetCollisionBoxesEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.annotation.Nullable;
+
+import com.mraof.minestuck.util.Debug;
 
 public class EntityBubble extends Entity
 {
@@ -19,6 +24,8 @@ public class EntityBubble extends Entity
 	private static final DataParameter<Integer> COLOR = EntityDataManager.createKey(EntityBubble.class, DataSerializers.VARINT);
 	private static final DataParameter<Boolean> CAN_ENTER = EntityDataManager.createKey(EntityBubble.class, DataSerializers.BOOLEAN);
 	private static final DataParameter<Boolean> CAN_EXIT = EntityDataManager.createKey(EntityBubble.class, DataSerializers.BOOLEAN);
+	
+	private static HashMap<EntityBubble, ArrayList<Entity>> stuck = new HashMap<>();
 
 	public EntityBubble(World worldIn)
 	{
@@ -53,13 +60,14 @@ public class EntityBubble extends Entity
 
 	}
 
-	/*
 	@Nullable
 	@Override
 	public AxisAlignedBB getCollisionBoundingBox() {
 		return canEnter() ? null : getEntityBoundingBox();
 	}
-	*/
+	
+	
+	
 	public float getSize()
 	{
 		return dataManager.get(SIZE);
@@ -86,32 +94,37 @@ public class EntityBubble extends Entity
 		return super.hitByEntity(entityIn);
 	}
 
-	/*
 	@SubscribeEvent
 	public static void onGetCollisionBoxes(GetCollisionBoxesEvent event)
 	{
 		Entity entity = event.getEntity();
 		if(entity != null && !(entity instanceof EntityBubble) && entity.world != null && event.getAabb() != null )
+		{
 			for(EntityBubble bubble : entity.world.getEntitiesWithinAABB(EntityBubble.class, event.getAabb()))
 			{
 				AxisAlignedBB aabb = bubble.getEntityBoundingBox();
-
-				if(!bubble.canExit())
+				if(!bubble.canExit()) 
 				{
-
-					//System.out.println(entity.posY + " " + aabb.minY);
-
-					//event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.minZ, aabb.minX, aabb.maxY, aabb.maxZ));
-					//if(entity.posY > aabb.minY)
-					//event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.minY, aabb.maxZ));
-					//event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.minZ));
-					//event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.maxX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ));
-					//event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.maxY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ));
-					//event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.maxZ, aabb.maxX, aabb.maxY, aabb.maxZ));
+					if(stuck.get(bubble) != null && stuck.get(bubble).contains(entity))
+					{
+						event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.minZ, aabb.minX, aabb.maxY, aabb.maxZ));
+						event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.minY, aabb.maxZ));
+						event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.minZ));
+						event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.maxX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ));
+						event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.maxY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ));
+						event.getCollisionBoxesList().add(new AxisAlignedBB(aabb.minX, aabb.minY, aabb.maxZ, aabb.maxX, aabb.maxY, aabb.maxZ));
+					}
+					else if((entity.posX >= aabb.minX && entity.posY >= aabb.minY && entity.posZ >= aabb.minZ && entity.posX <= aabb.maxX && entity.posY <= aabb.maxY && entity.posZ <= aabb.maxZ))
+					{
+						if(stuck.get(bubble) == null)
+							stuck.put(bubble, new ArrayList<Entity>());
+						if(!stuck.get(bubble).contains(entity))
+							stuck.get(bubble).add(entity);
+					}
 				}
 			}
+		}
 	}
-	*/
 
 	public int getColor() {
 		return dataManager.get(COLOR);
