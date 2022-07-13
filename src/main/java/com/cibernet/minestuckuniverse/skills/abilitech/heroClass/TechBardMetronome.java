b028ab902afd9796f1acdd43863a04cb68a84ed4
grandioseComputer <garbedcrustacean@gmail.com>
2022-07-13 11:58:36 -0500

    classes

--- src/main/java/com/cibernet/minestuckuniverse/skills/abilitech/heroClass/TechBardMetronome.java
+++ src/main/java/com/cibernet/minestuckuniverse/skills/abilitech/heroClass/TechBardMetronome.java
@@ -1,7 +1,13 @@
 package com.cibernet.minestuckuniverse.skills.abilitech.heroClass;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.cibernet.minestuckuniverse.capabilities.badgeEffects.IBadgeEffects;
 import com.cibernet.minestuckuniverse.capabilities.keyStates.SkillKeyStates;
+import com.cibernet.minestuckuniverse.capabilities.keyStates.SkillKeyStates.KeyState;
+import com.cibernet.minestuckuniverse.skills.MSUSkills;
+import com.cibernet.minestuckuniverse.skills.abilitech.Abilitech;
 import com.mraof.minestuck.util.EnumClass;
 
 import net.minecraft.entity.player.EntityPlayer;
@@ -17,9 +23,24 @@ public class TechBardMetronome  extends TechHeroClass
 	@Override
 	public boolean onUseTick(World world, EntityPlayer player, IBadgeEffects badgeEffects, int techSlot, SkillKeyStates.KeyState state, int time)
 	{
+		if(state == KeyState.NONE)
+			return false;
+		
+		if(badgeEffects.getExternalTech(techSlot) == null)
+		{
+			ArrayList<Abilitech> POOL = new ArrayList<Abilitech>();
+			POOL.addAll(ABILITECHS);
+			
+		}
+		
+		
 		
 		
 		
 		return true;
 	}
+	
+	public static class slotA {}
+	public static class slotB {}
+	public static class slotC {}
 }
