e94cf857db245779559cd5fb9a969392e27282e8
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2022-07-13 21:02:47 -0400

    buffed R2D

--- src/main/java/com/cibernet/minestuckuniverse/skills/abilitech/heroAspect/voidAspect/TechVoidSnap.java
+++ src/main/java/com/cibernet/minestuckuniverse/skills/abilitech/heroAspect/voidAspect/TechVoidSnap.java
@@ -1,8 +1,10 @@
 package com.cibernet.minestuckuniverse.skills.abilitech.heroAspect.voidAspect;
 
+import com.cibernet.minestuckuniverse.MSUConfig;
 import com.cibernet.minestuckuniverse.capabilities.keyStates.SkillKeyStates;
 import com.cibernet.minestuckuniverse.capabilities.MSUCapabilities;
 import com.cibernet.minestuckuniverse.capabilities.badgeEffects.IBadgeEffects;
+import com.cibernet.minestuckuniverse.events.AbilitechTargetedEvent;
 import com.cibernet.minestuckuniverse.particles.MSUParticles;
 import com.cibernet.minestuckuniverse.skills.abilitech.heroAspect.TechHeroAspect;
 import com.cibernet.minestuckuniverse.util.EnumTechType;
@@ -14,11 +16,13 @@ import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class TechVoidSnap extends TechHeroAspect
 {
-	public TechVoidSnap(String name) {
-		super(name, EnumAspect.VOID, EnumTechType.DEFENSE, EnumAspect.DOOM);
+	public TechVoidSnap(String name, long cost) {
+		super(name, EnumAspect.VOID, cost, EnumTechType.UTILITY);
 	}
 
 	protected static final int ENERGY_USE = 2;
@@ -38,7 +42,8 @@ public class TechVoidSnap extends TechHeroAspect
 		EntityLivingBase target = MSUUtils.getTargetEntity(player);
 		badgeEffects.startPowerParticles(getClass(), MSUParticles.ParticleType.AURA, EnumAspect.VOID, 5);
 
-		if(target instanceof IMob || target instanceof EntityMinestuck)
+		if(target != null && !(target instanceof EntityPlayer) && !MSUConfig.entityPowerProtection.contains(EntityRegistry.getEntry(target.getClass()).getRegistryName().toString()) &&
+				!MinecraftForge.EVENT_BUS.post(new AbilitechTargetedEvent(world, player, this, techSlot, false)))
 		{
 			target.setDead();
 			if (!player.isCreative() && !world.isRemote)
@@ -47,5 +52,11 @@ public class TechVoidSnap extends TechHeroAspect
 		}
 
 		return true;
+	}
+
+	@Override
+	public boolean isUsableExternally(World world, EntityPlayer player)
+	{
+		return super.isUsableExternally(world, player) && player.getFoodStats().getFoodLevel() >= ENERGY_USE;
 	}
 }
