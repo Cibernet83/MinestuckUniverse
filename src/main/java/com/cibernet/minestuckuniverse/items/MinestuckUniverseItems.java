3d64c7a9e37c7c7e81003f4b626548356040c9c8
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2022-07-27 20:36:55 -0400

    downloaded css for aux key

--- src/main/java/com/cibernet/minestuckuniverse/items/MinestuckUniverseItems.java
+++ src/main/java/com/cibernet/minestuckuniverse/items/MinestuckUniverseItems.java
@@ -910,6 +910,7 @@ public class MinestuckUniverseItems
         registerItemCustomRender(cruxiteGel, new MSUModelManager.SubtypesItemDefinition("cruxite_gel"));
         registerItemCustomRender(cruxtruderGel, new MSUModelManager.SubtypesItemDefinition("cruxtruder_gel"));
         registerItemCustomRender(captchalogueBook, new MSUModelManager.SubtypesItemDefinition("captchalogue_book"));
+        registerItemCustomRender(chastityKey, new MSUModelManager.SubtypesItemDefinition("chasity_key"));
         registerItemCustomRender(ceramicPorkhollow, new MSUModelManager.SubtypesItemDefinition("ceramic_porkhollow"));
 
         registerItemCustomRender(gtHood, new MSUModelManager.GodTierArmorItemDefinition((ItemGTArmor) gtHood));
